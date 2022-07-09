c807e7015a47ab2eda08ff94578ddbf089f860c2
IchiKaru <04johnraymond@gmail.com>
2022-07-09 14:29:41 +0800

    Phase 2 Re-editing

--- Document_creation.py
+++ Document_creation.py
@@ -1,9 +1,9 @@
-import time
 from docx import *
 from docx.enum.style import *
 from docx.shared import *
-from docx.oxml.ns import qn
+from docx.oxml.ns import *
 from docx.enum.section import WD_SECTION
+from docx.enum.text import *
 
 document = Document()
 sections = document.sections
@@ -21,21 +21,40 @@ cols = sectPR.xpath('./w:cols')[0]
 cols.set(qn('w:num'), '1')
 
 document.add_heading('Balance Sheet', 0)
-s = document.sections[0]
-s.start_type = WD_SECTION
-
 
 global content
 content = 'Hello, thank you for using Py-accounting software. You May Delete this Message.'
 
-def createdocx():
-    cols.set(qn('w:num'), '4')
-    p = document.add_paragraph(content)
-    p.style = document.styles.add_style('Default', WD_STYLE_TYPE.PARAGRAPH)
-    font = p.style.font
-    font.name = ('Century Gothic')
-    font.size = Pt(11.5)
-
-createdocx()
+def writedocx(content, font_name = 'Times New Roman', font_size = 12, font_bold = False, font_italic = False, font_underline = False, color = RGBColor(0, 0, 0),
+              before_spacing = 5, after_spacing = 5, line_spacing = 1.5, keep_together = True, keep_with_next = False, page_break_before = False,
+              widow_control = False, align = 'left', style = ''):
+    paragraph = document.add_paragraph(str(content))
+    paragraph.style = document.styles.add_style(style, WD_STYLE_TYPE.PARAGRAPH)
+    font = paragraph.style.font
+    font.name = font_name
+    font.size = Pt(font_size)
+    font.bold = font_bold
+    font.italic = font_italic
+    font.underline = font_underline
+    font.color.rgb = color
+    paragraph_format = paragraph.paragraph_format
+    paragraph_format.space_before = Pt(before_spacing)
+    paragraph_format.space_after = Pt(after_spacing)
+    paragraph.line_spacing = line_spacing
+    paragraph_format.keep_together = keep_together
+    paragraph_format.keep_with_next = keep_with_next
+    paragraph_format.page_break_before = page_break_before
+    paragraph_format.widow_control = widow_control
+    if align.lower() == 'left':
+        paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+    elif align.lower() == 'center':
+        paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+    elif align.lower() == 'right':
+        paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
+    elif align.lower() == 'justify':
+        paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
+    else:
+        paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+writedocx(content)
 
 document.save('Word.docx')
--- Main.py
+++ Main.py
@@ -1,9 +1,12 @@
-#pylint:disable=E0602
-#A program that records user input labelling it as either Assets , Liabilities, or Equity.
+#phase 1 of the project
 
 import time
 from docx import *
 from docx.enum.style import *
+from docx.shared import *
+from docx.oxml.ns import *
+from docx.enum.section import WD_SECTION
+from docx.enum.text import *
 time.sleep(0.5)
 
 time.sleep(1)
@@ -315,7 +318,62 @@ def Retry_parameter(): #Only appears when the recorded values are correct and as
         print('-'*20)
         for a, b in zip(Withdrawal, Withdrawal_value):
             print(a, b)
-        
+
+#Phase 2 of The Project
+def phase2():
+    document = Document()
+    sections = document.sections
+    for section in sections:
+        section.top_margin = Inches(0.5)
+        section.bottom_margin = Inches(0.5)
+        section.left_margin = Inches(1)
+        section.right_margin = Inches(1)
+
+    section = document.sections[0]
+
+    sectPR =  section._sectPr
+    cols = sectPR.xpath('./w:cols')[0]
+
+    cols.set(qn('w:num'), '1')
+
+    document.add_heading('Balance Sheet', 0)
+
+    global content
+    content = 'Hello, thank you for using Py-accounting software. You May Delete this Message.'
+
+    def writedocx(content, font_name = 'Times New Roman', font_size = 12, font_bold = False, font_italic = False, font_underline = False, color = RGBColor(0, 0, 0),
+                before_spacing = 5, after_spacing = 5, line_spacing = 1.5, keep_together = True, keep_with_next = False, page_break_before = False,
+                widow_control = False, align = 'left', style = ''):
+        paragraph = document.add_paragraph(str(content))
+        paragraph.style = document.styles.add_style(style, WD_STYLE_TYPE.PARAGRAPH)
+        font = paragraph.style.font
+        font.name = font_name
+        font.size = Pt(font_size)
+        font.bold = font_bold
+        font.italic = font_italic
+        font.underline = font_underline
+        font.color.rgb = color
+        paragraph_format = paragraph.paragraph_format
+        paragraph_format.space_before = Pt(before_spacing)
+        paragraph_format.space_after = Pt(after_spacing)
+        paragraph.line_spacing = line_spacing
+        paragraph_format.keep_together = keep_together
+        paragraph_format.keep_with_next = keep_with_next
+        paragraph_format.page_break_before = page_break_before
+        paragraph_format.widow_control = widow_control
+        if align.lower() == 'left':
+            paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+        elif align.lower() == 'center':
+            paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+        elif align.lower() == 'right':
+            paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
+        elif align.lower() == 'justify':
+            paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
+        else:
+            paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+    writedocx(content) 
+
+    document.save('Word.docx')
 
 variables()
 start_program()
@@ -347,24 +405,6 @@ if check=='y':
             elif seeif=='n':
                 print('ok.')
 else:
-    print("Your recorded values are now gone. Sorry, this is a work in progress. Hahaha!") 
-#Phase 2 of the project 
-document = Document()
-
-document.add_heading('Balance Sheet', 0)
-
-p = document.add_paragraph( 'Hello there, this is a test document from Py-Accounting Software in trial mode. You are seeing this message because it is the defualt. It will be removed once all necessary to do things are completed. Otherwise, you ')
-p.add_run('may or may not delete ').bold = True
-p.add_run('this message.')
+    phase2()
 
 
-
-
-
-document.save = Document(r'C:\Users\Student\Documents\Variable-docx(es)\TestDemo.docx')
-
-
-#Test Add ons
-"""p.add_run('bold').bold = True
-p.add_run(' and some ') 
-p.add_run('italic.').italic = True"""
